3ae2153237d8a2f00df67c86708756ef0fa549ac
Ben Hernandez <benjammin234@gmail.com>
2020-02-13 11:19:38 -0800

    Creates the app form and book component

---
+++ src/Book.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Moment from "moment";
+import moment from "moment";
+
+export default function Book(props) {
+  return (
+    <div className="Book">
+      <h2>{props.title}</h2>
+      <div className="Book_author">by {props.authoer}</div>
+      <div className="Book_publisher">
+        Published by: {props.publisher} on{" "}
+        {moment(props.published_date).format("DD MMM YYYY")}
+      </div>
+      <div className="Book_description">{props.description}</div>
+      <div className="Book_details">Rank {props.rank} this week</div>
+    </div>
+  );
+}
